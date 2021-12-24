20f0ee7dfdcc329ac5f448dc1df572eacf6dc2a5
unknown <892368376@qq.com>
2021-12-24 20:11:57 +0800

    2021年12月24日20:11:56

--- src/CatVm2/tools/vm_proxy.js
+++ src/CatVm2/tools/vm_proxy.js
@@ -1,14 +1,20 @@
-
 //框架代理功能
-catvm.proxy = function (o) {
+catvm.proxy = function(o) {
     if (catvm.memory.config.proxy == false) { return o };
     return new Proxy(o, {
-        set(obj, prop, value) {
-            console.log("set", obj, prop, value);
+        set(target, property, value) {
+            // console.log("set", obj, prop, value);
+
+
+            console.table([{ "类型": "set-->", "调用者": target, "属性": property, "值": value }])
+
             return Reflect.set(...arguments);
         },
-        get: function (target, property, receiver) {
-            console.log("get", target, property, target[property])
+        get: function(target, property, receiver) {
+            // console.log("get", target, property, target[property])
+
+            console.table([{ "类型": "set-->", "调用者": target, "属性": property, "值": target[property] }])
+
             return target[property];
         }
     })
